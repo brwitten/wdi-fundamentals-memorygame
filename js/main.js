7edea2a052f1579e5a412117dabe621af5c44316
Bethany Witten <bethrwitten@Bethanys-MacBook-Pro.local>
2017-08-28 20:28:42 -0700

    created logic game

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!");
-	} else {
-		alert("Sorry, try again!");
-	}
+var checkForMatch = function() {
+	  if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
 }
+
+var flipCard = function(cardId) {
+  checkForMatch();
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+	}
+
+flipCard(0);
+flipCard(2);
