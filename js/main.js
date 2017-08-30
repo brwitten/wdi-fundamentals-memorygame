f9aca8147f48e12c638d066fc626c7d30486ecca
Bethany Witten <bethrwitten@Bethanys-MacBook-Pro.local>
2017-08-29 20:34:56 -0700

    created logic game

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	  if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
   	} else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 };
 
